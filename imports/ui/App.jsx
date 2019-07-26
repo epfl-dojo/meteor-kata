452de1e10d8ac88a83653f164e1f14456742ac88
Loïc Humbert <loichumbert5450@gmail.com>
2019-07-26 13:44:44 +0200

    Two Hello components synchronized

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Hello from './Hello.jsx';
 import Knights from './Knights.jsx';
+import { withTracker } from 'meteor/react-meteor-data';
+
 
 const testData = [
     {name: "Lancelot", quest: "To seek the Holy Grail", favoriteColor: "blue"},
@@ -8,12 +10,25 @@ const testData = [
     {name: "Galahad", quest: "To seek the Holy Grail", unladenSwallowVelocity: 0}
 ]
 
-const App = () => (
-  <div>
-    <h1>Exemple avec React</h1>
-    <Hello />
-    <Knights testData={testData} />
-  </div>
-);
+counter = new ReactiveVar(0)
 
-export default App;
+class App extends Component{
+  increment() {
+    counter.set(counter.get() + 1);
+  }
+
+  render() {
+    return <div>
+    <h1>Exemple avec React</h1>
+    <Hello counter={this.props.counter} increment={this.increment.bind(this)}/>
+    <Hello counter={this.props.counter} increment={this.increment.bind(this)}/>
+    <Knights testData={testData} />
+  </div>;
+  }
+}
+
+export default AppWrapped = withTracker(() => {
+    return {
+      counter: counter.get()
+    };
+})(App);
--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -1,22 +1,15 @@
 import React, { Component } from 'react';
 
-export default class Hello extends Component {
-  state = {
-    counter: 0,
-  }
 
-  increment() {
-    this.setState({
-      counter: this.state.counter + 1
-    });
-  }
-
+class Hello extends Component {
   render() {
     return (
       <div>
-        <p>Vous avez pressé le bouton {this.state.counter} fois.</p>
-        <button onClick={() => this.increment()}>Cliquez-moi !</button>
+        <p>Vous avez pressé le bouton {this.props.counter} fois.</p>
+        <button onClick={() => this.props.increment()}>Cliquez-moi !</button>
       </div>
     );
   }
 }
+
+export default Hello;
